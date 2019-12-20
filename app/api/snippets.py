7516c972fecce1f303d0f9ae72c80f2eaa7b5baf
Marcelo Salloum <marcelosalloum@gmail.com>
2019-12-20 16:31:19 -0300

    Implemented snippet creation request

--- app/api/snippets.py
+++ app/api/snippets.py
@@ -1,9 +1,10 @@
 from app import db
-from marshmallow import fields
 from app.api import bp
-from app.models import Snippet, SnippetScheme
-from flask import abort, g, jsonify, request, make_response
 from app.api.errors import bad_request, forbidden_error
+from app.models import Snippet, SnippetScheme
+from dateutil.parser import parse
+from flask import abort, g, jsonify, request, make_response, url_for
+from marshmallow import fields
 
 
 @bp.route('/snippets/<int:id>', methods=['GET'])
@@ -13,8 +14,35 @@ def get_snippet(id):
     return jsonify(snippet_schema.dump(snippet))
 
 
-# - POST request to create the snippet: 'name', 'snippet', 'expires'
-#       - The request to store the snippet should be replied to with a response that 
+@bp.route('/snippets', methods=['POST'])
+def create_snippet():
+    data = request.get_json() or {}
+
+    # Validation
+    if 'name' not in data or 'snippet' not in data or 'expires' not in data:
+        return bad_request("The fields 'name', 'snippet' and 'expires' are mandatory!")
+
+    try:
+        parse(data['expires'])
+    except:
+        return bad_request("The 'expires' field couldn't be parsed to a valid datetime. To prevent this, make sure to adopt a widely known datetime format, like ISO 8601 (YYYY-MM-DDTHH:MM:SS.mmmmmm)")
+
+    # Object creation
+    snippet_scheme = SnippetScheme()
+    snippet = Snippet()
+    snippet_scheme.load(data, instance=snippet, partial=True)
+    db.session.add(snippet)
+    db.session.commit()
+
+    # Response
+    response = jsonify(snippet_scheme.dump(snippet))
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.get_snippet', id=snippet.id)
+    return response
+
+
+# - OK: POST request to create the snippet: 'name', 'snippet', 'expires'
+#       - OK: The request to store the snippet should be replied to with a response that
 #       includes the URL where the snippet can be read.
 # - OK: GET
 #       - Snippets expiry should be extended when they are accessed.
