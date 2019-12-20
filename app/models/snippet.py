39775080df53d1e1cd07dda2fd4435391ef74b96
Marcelo Salloum <marcelosalloum@gmail.com>
2019-12-20 16:30:54 -0300

    Fixed migration

--- app/models/snippet.py
+++ app/models/snippet.py
@@ -5,8 +5,8 @@ from marshmallow import EXCLUDE
 
 class Snippet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    snippet = db.Column(db.String(1000000), index=True, unique=True)
+    name = db.Column(db.String(64))
+    snippet = db.Column(db.String(1000000))
     expires = db.Column(db.DateTime)
 
     def __repr__(self):
@@ -15,10 +15,10 @@ class Snippet(db.Model):
 class SnippetScheme(ma.ModelSchema):
     class Meta:
         model = Snippet
-        fields = ('id', 'name', 'snippet', 'expires')
+        fields = ('id', 'name', 'snippet', 'expires', '_links')
         unknown = EXCLUDE
 
     # Smart hyperlinking
     _links = ma.Hyperlinks(
-        {"self": ma.URLFor("api.get_user", id="<id>")}
+        {"self": ma.URLFor("api.get_snippet", id="<id>")}
     )
